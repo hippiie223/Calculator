c3fbc83e808e32110e201051ae1704118c3b4a41
didi <reckywangbowen_i@didichuxing.com>
2019-01-09 16:03:40 +0800

    1.0个税补

---
+++ src/main/java/com/recky/calculator/common/Constant.java
@@ -0,0 +1,33 @@
+package com.recky.calculator.common;
+
+/**
+ * Created by reckywangbowen_i on 2019/01/09
+ */
+public class Constant {
+    public static final double TAX_1 = 0.03;
+    public static final double TAX_2 = 0.1;
+    public static final double TAX_3 = 0.2;
+    public static final double TAX_4 = 0.25;
+    public static final double TAX_5 = 0.3;
+    public static final double TAX_6 = 0.35;
+    public static final double TAX_7 = 0.45;
+
+    public static final int SHORTCUN_NUM_1 = 0;
+    public static final int SHORTCUN_NUM_2 = 2520;
+    public static final int SHORTCUN_NUM_3 = 16920;
+    public static final int SHORTCUN_NUM_4 = 31920;
+    public static final int SHORTCUN_NUM_5 = 52920;
+    public static final int SHORTCUN_NUM_6 = 85920;
+    public static final int SHORTCUN_NUM_7 = 181920;
+
+    public static final int CLASS_1 = 36000;
+    public static final int CLASS_2 = 144000;
+    public static final int CLASS_3 = 300000;
+    public static final int CLASS_4 = 420000;
+    public static final int CLASS_5 = 660000;
+    public static final int CLASS_6 = 960000;
+
+    public static final int TAX_START = 5000;
+
+
+}
